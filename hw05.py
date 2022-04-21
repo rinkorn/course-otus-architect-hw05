2bc12dddf82ca3313006aadc06e77d9d9ccdd9eb
AlbertM <rinkorn@mail.ru>
2022-04-21 13:01:18 +0300

    Update

--- hw05.py
+++ hw05.py
@@ -1,31 +1,103 @@
 # %%
+import abc
 import math
 
 
-class Movable:
-    def __init__(self, position, velocity):
-        self.position = position
-        self.velocity = velocity
+class Vector:
+    def __init__(self, data=[]):
+        if not isinstance(data, (type(self), list)):
+            raise TypeError('Wrong type of vector data.')
+        if len(data) != 2:
+            raise ValueError('Data length must be 2.')
+        self.data = data
 
+    def __len__(self):
+        return len(self.data)
+
+    def __add__(self, other):
+        if not isinstance(other, type(self)):
+            raise TypeError('Wrong type for add operation')
+        self.data = [i+j for i, j in zip(self.data, other.data)]
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __eq__(self, other: object) -> bool:
+        return self.data == other.data
+
+    def __call__(self):
+        return self.data
+
+    def __str__(self) -> str:
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+def isVectorType(value):
+    if isinstance(value, Vector):
+        return True
+    else:
+        raise TypeError("Wrong type. Must be 'Vector'.")
+
+
+class Movable(abc.ABC):
+    @abc.abstractmethod
     def get_position(self):
-        return self.position
+        pass
 
+    @abc.abstractmethod
     def get_velocity(self):
-        return self.velocity
+        pass
 
-    def set_position(self, new_position):
-        self.position = new_position
+    @abc.abstractmethod
+    def set_position(self):
+        pass
 
 
 class Move(Movable):
+    def __init__(self, position: Vector, velocity: Vector):
+        if isVectorType(position):
+            self.position = position
+        if isVectorType(velocity):
+            self.velocity = velocity
+
+    def get_position(self):
+        try:
+            return self.position
+        except:
+            raise ValueError("Can't get position")
+
+    def get_velocity(self):
+        try:
+            return self.velocity
+        except:
+            raise ValueError("Can't get velocity")
+
+    def set_position(self, new_position):
+        if isVectorType(new_position):
+            self.position = new_position
+
     def execute(self):
-        p = self.get_position()
-        v = self.get_velocity()
-        new_p = list([ip + iv for ip, iv in zip(p, v)])
-        self.set_position(new_p)
+        position = self.get_position()
+        velocity = self.get_velocity()
+        new_position = position + velocity
+        self.set_position(new_position)
 
 
-class Rotable:
+class Rotable(abc.ABC):
+    @abc.abstractmethod
+    def get_direction(self):
+        pass
+
+    @abc.abstractmethod
+    def set_direction(self):
+        pass
+
+
+class Rotate(Rotable):
     def __init__(self, direction, angular_velocity, direction_numbers):
         self.direction = direction
         self.angular_velocity = angular_velocity
@@ -37,8 +109,6 @@ class Rotable:
     def set_direction(self, new_direction):
         self.direction = new_direction
 
-
-class Rotate(Rotable):
     def execute(self):
         d = self.get_direction()
         v = self.angular_velocity()
@@ -46,19 +116,33 @@ class Rotate(Rotable):
         self.set_direction(new_direction)
 
 
-class UObject:
+class UObject(abc.ABC):
+    @abc.abstractmethod
     def get_property(self, attr):
-        return self.__dict__[attr]
+        # return self.__dict__[attr]
+        pass
 
+    @abc.abstractmethod
     def set_property(self, attr, value):
-        self.__dict__[attr] = value
+        # self.__dict__[attr] = value
+        pass
 
 
 class MovableAdapter(Movable, Rotable, UObject):
     def __init__(self, position, velocity, direction, angular_velocity, direction_numbers):
-        Movable.__init__(self, position, velocity)
-        Rotable.__init__(self, direction, angular_velocity, direction_numbers)
-        UObject.__init__(self)
+        self.position = position
+        self.velocity = velocity
+        self.direction = direction
+        self.angular_velocity = angular_velocity
+        self.direction_numbers = direction_numbers
+
+    def get_property(self, attr):
+        # return super().get_property(attr)
+        return self.__dict__[attr]
+
+    def set_property(self, attr, value):
+        # return super().set_property(attr, value)
+        self.__dict__[attr] = value
 
     def get_position(self):
         return self.get_property('position')
@@ -72,29 +156,28 @@ class MovableAdapter(Movable, Rotable, UObject):
         v = self.get_property('velocity')
 
         d = float(d)
-        # print(f"direction: {d}")
-        # print(f"direction_numbers: {dn}")
-        # print(f"before cos: {d / (2 * math.pi) * dn}")
-        # print(f"before sin: {d / (2 * math.pi) * dn}")
-        # print(f"cos: {math.cos(d / (2 * math.pi) * dn)}")
-        # print(f"sin: {math.sin(d / (2 * math.pi) * dn)}")
-
         v0 = v[0] * math.cos(d / (2 * math.pi) * dn)
         v1 = v[1] * math.sin(d / (2 * math.pi) * dn)
         new_v = [v0, v1]
         return new_v
 
+    def get_direction(self):
+        return super().get_direction()
+
+    def set_direction(self):
+        return super().set_direction()
+
 
 if __name__ == "__main__":
-    position = p = [12, 5]
-    velocity = v = [-7, 3]
+    position = p = Vector([12, 5])
+    velocity = v = Vector([-7, 3])
     direction = d = 0
     angular_velocity = av = 0
     direction_numbers = dn = 8
     move = Move(p, v)
     move.execute()
     print(move.get_position())
-    m = MovableAdapter(p, v, d, av, dn)
-    print(m.get_position())
-    print(m.get_velocity())
+    # m = MovableAdapter(p, v, d, av, dn)
+    # print(m.get_position())
+    # print(m.get_velocity())
     # m.set_position([1, 1])
--- test_hw05.py
+++ test_hw05.py
@@ -1,4 +1,5 @@
 import pytest
+from hw05 import Vector
 from hw05 import Movable, Move
 from hw05 import Rotable, Rotate
 from hw05 import UObject
@@ -6,12 +7,40 @@ from hw05 import MovableAdapter
 
 
 def test_Move_0():
-    position = [12, 5]
-    velocity = [-7, 3]
+    position = Vector([12, 5])
+    velocity = Vector([-7, 3])
     m = Move(position, velocity)
     m.execute()
-    new_position = m.get_position()
-    assert new_position == [5, 8]
+    new_postition = m.get_position()
+    expected_position = Vector([5, 8])
+    assert m.get_position() == expected_position
+
+
+def test_Move_1(mocker):
+    position = None
+    velocity = Vector([-7, 3])
+    with pytest.raises(Exception):
+        m = Move(position, velocity)
+        m.execute()
+
+
+def test_Move_2(mocker):
+    position = Vector([12, 5])
+    velocity = None
+    with pytest.raises(Exception):
+        m = Move(position, velocity)
+        m.execute()
+
+
+def test_Move_3():
+    position = Vector([12, 5])
+    velocity = Vector([-7, 3])
+    m = Move(position, velocity)
+    # m.execute()
+    with pytest.raises(Exception):
+        m.set_position([25, -1])
+        # m.set_position(Vector([25, -1]))
+
 
 # def test_Move_1(mocker):
 #     position = [0, 0]
